27d5bcb20ffb65dc3eb77a92803cbb73bfbd2891
Seda <sedan.taskan@gmail.com>
2024-08-14 14:46:49 +0300

    commit for save again

--- numberConverter/src/main/java/org/example/language/NumberTranslatorTr.java
+++ numberConverter/src/main/java/org/example/language/NumberTranslatorTr.java
@@ -12,7 +12,7 @@ public class NumberTranslatorTr implements NumberTranslator {
     public String translate(int input) {
 
         String[] ones = {"", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
-        String[] tens = {"on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
+        String[] tens = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
         String[] postfix = {"", "yüz", "bin", "milyon", "milyar", "trilyon"};
 
         String numberAsString = String.valueOf(input);
@@ -25,24 +25,46 @@ public class NumberTranslatorTr implements NumberTranslator {
             numberAsString = "0" + numberAsString;
         }
 
-        String[] coupleOfDigits =  numberAsString.split("(?<=\\G.{"+3+"})");
-        System.out.println("coupleOfDigits: " + Arrays.toString(coupleOfDigits));
+        List<String> coupleOfDigits = List.of(numberAsString.split("(?<=\\G.{" + 3 + "})"));
+
+        List<String> reversed = new ArrayList<>();
+
+        for (int i = coupleOfDigits.size(); i > 0; i--) {
+            reversed.add(coupleOfDigits.get(i - 1));
+        };
+
+        coupleOfDigits = reversed;
 
         List<String> numberAsStr = new ArrayList<>();
 
         int hundred = 0;
         int ten = 0;
         int one = 0;
+        String power = "";
 
-        for (int i = 0; i<coupleOfDigits.length ; i++){
-            System.out.println(coupleOfDigits[i]);
-            hundred = Character.getNumericValue(coupleOfDigits[i].charAt(0));
-            System.out.println(hundred);
-            ten = Character.getNumericValue(coupleOfDigits[i].charAt(1));
-            one = Character.getNumericValue(coupleOfDigits[i].charAt(2));
-            String power = postfix[i];
+        for (int i = 0; i<coupleOfDigits.size() ; i++){
+            hundred = Character.getNumericValue(coupleOfDigits.get(i).charAt(0));
+            ten = Character.getNumericValue(coupleOfDigits.get(i).charAt(1));
+            one = Character.getNumericValue(coupleOfDigits.get(i).charAt(2));
 
-            numberAsStr.add(ones[hundred] + " " + power + " " + tens[ten] + " " + ones[one]);
+            if(i == 1){
+                power = postfix[2];
+            } else if (i == 2) {
+                power = postfix[3];
+            } else if (i == 3) {
+                power = postfix[4];
+            } else if (i == 4) {
+                power = postfix[5];
+            }
+
+            String hund = "";
+            if(hundred == 0){
+                hund = "";
+            }else {
+                hund = postfix[1];
+            }
+
+            numberAsStr.addFirst(ones[hundred] + " " + hund + " " + tens[ten] + " " + ones[one] + " " + power);
         }
 
 
@@ -51,6 +73,10 @@ public class NumberTranslatorTr implements NumberTranslator {
             translatedNumber.append(ch.trim()).append(" ");
         }
 
-        return translatedNumber.toString().replaceAll("\\s+"," ").trim();
+        return translatedNumber.toString()
+                .replaceAll("\\s+"," ")
+                .trim()
+                .replace("bir yüz", "yüz")
+                .replace("bir bin", "bin");
     }
 }
