47bff7e4bcb866c9b6f677f9a741f631ba7a93ee
Seda <sedan.taskan@gmail.com>
2024-08-14 13:26:23 +0300

    changes on logic

--- numberConverter/src/main/java/org/example/language/NumberTranslatorTr.java
+++ numberConverter/src/main/java/org/example/language/NumberTranslatorTr.java
@@ -2,6 +2,8 @@ package org.example.language;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NumberTranslatorTr implements NumberTranslator {
 
@@ -12,31 +14,21 @@ public class NumberTranslatorTr implements NumberTranslator {
         String[] tens = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
         String[] power = {"", "on", "yüz", "bin", "milyon", "milyar", "trilyon"};
 
+        String numberAsString = String.valueOf(input);
+
         int digitNumber = (int)Math.log10(input) + 1;
+
+        if (digitNumber % 3 == 1){
+            numberAsString = "00" + numberAsString;
+        }else if (digitNumber % 3 == 2){
+            numberAsString = "0" + numberAsString;
+        }
+
+        String[] coupleOfDigits =  numberAsString.split("(?<=\\G.{"+3+"})");
 
         List<String> numberAsStr = new ArrayList<>();
 
-        switch(digitNumber){
-            case 1:
-                numberAsStr.add(ones[input]);
-                break;
-            case 2:
-                numberAsStr.add(tens[input/10] + " " + ones[input % 10]);
-                break;
-            case 3:
-                numberAsStr.add(ones[(input/100)] + " " + power[2] + " " + tens[(input/10)%10] + " " + ones[input%10]);
-                break;
-            case 4:
-                numberAsStr.add(ones[(input/1000)] + " " + power[3] + " " + ones[(input/100)%10] + " " + power[(input/100)%10] + " " + tens[(input/10)%10] + " " + ones[input%10]);
-                break;
-            default:
-                if (input == 0){
-                    numberAsStr.add("sıfır");
-                } else{
-                    numberAsStr.add("Bir şeyler ters gitti");
-                }
-                break;
-        }
+
 
         StringBuilder translatedNumber = new StringBuilder();
         for (String ch : numberAsStr) {
