30a2d72127cd044fd911bfc6dfc30cdd01187e4e
Seda <sedan.taskan@gmail.com>
2024-08-18 23:30:05 +0300

    change on GlobalExceptionHandler

--- carInventory/src/main/java/org/example/carinventory/exception/GlobalExceptionHandler.java
+++ carInventory/src/main/java/org/example/carinventory/exception/GlobalExceptionHandler.java
@@ -1,12 +1,21 @@
 package org.example.carinventory.exception;
 
+
+
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Handler.class);
     @ExceptionHandler({NoCarOnInventoryException.class})
     public ResponseEntity<?> handleNoCarOnInventoryException(NoCarOnInventoryException e) {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(e.getMessage());
@@ -24,7 +33,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<?> handleException(Exception e) {
-        //TODO: log to console
+        //TODO: log to console *
+        LOGGER.log(Level.SEVERE, "An exception occurred: ", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An exception occurred: ");
     }
 
